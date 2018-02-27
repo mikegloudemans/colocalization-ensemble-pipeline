12cbd0267f312c86e7885150d3843b1d5a70e379
mikegloudemans <mgloud@durga.stanford.edu>
2018-02-27 10:52:34 -0800

    Minor RPE sQTL bug fix

--- scripts/data_prep/rpe/rpe_sqtl_only.sh
+++ scripts/data_prep/rpe/rpe_sqtl_only.sh
@@ -11,7 +11,7 @@ bgzip -f $sqtl_dir/rpe_glucose_sqtls.txt
 tabix -S 1 -s 2 -b 3 -e 3 $sqtl_dir/rpe_glucose_sqtls.txt.gz
 
 zcat /srv/persistent/bliu2/rpe/processed_data/sqtl/fastQTL/nominal/galactose/all.nominal.txt.gz | sed s/chr//g | sed s/_/\\t/g | grep -v nan | awk '{print $1 "_" $2 "\t" $0}' | cut -f2,3 --complement | sort -k2,2 -k3,3n >> $sqtl_dir/rpe_galactose_sqtls.txt
-bgzip -f $sqtl_dir/rpe_sqtls.txt
+bgzip -f $sqtl_dir/rpe_galactose_sqtls.txt
 tabix -S 1 -s 2 -b 3 -e 3 $sqtl_dir/rpe_galactose_sqtls.txt.gz
 
 

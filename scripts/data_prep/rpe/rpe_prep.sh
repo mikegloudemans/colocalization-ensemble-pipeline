197bba91d6e1504dbdc7d2a3103741ace9eb8075
mikegloudemans <mgloud@durga.stanford.edu>
2018-02-27 10:15:25 -0800

    Added galactose sQTLs for RPE splicing analysis

--- scripts/data_prep/rpe/rpe_prep.sh
+++ scripts/data_prep/rpe/rpe_prep.sh
@@ -27,10 +27,14 @@ tabix -p bed -S 1 -s 3 -b 4 /users/mgloud/projects/brain_gwas/data/eqtls/output/
 # sQTL data
 sqtl_dir=/users/mgloud/projects/brain_gwas/data/eqtls/rpe/sqtls
 header="feature\tchr\tsnp_pos\tref\talt\tbuild\tfeature_distance\tpvalue\tbeta\tse"
-echo -e $header > $sqtl_dir/rpe_sqtls.txt
+echo -e $header > $sqtl_dir/rpe_glucose_sqtls.txt
+echo -e $header > $sqtl_dir/rpe_galactose_sqtls.txt
 
-zcat /srv/persistent/bliu2/rpe/processed_data/sqtl/fastQTL/nominal/all.nominal.txt.gz | sed s/chr//g | sed s/_/\\t/g | grep -v nan | awk '{print $1 "_" $2 "\t" $0}' | cut -f2,3 --complement | sort -k2,2 -k3,3n >> $sqtl_dir/rpe_sqtls.txt
+zcat /srv/persistent/bliu2/rpe/processed_data/sqtl/fastQTL/nominal/glucose/all.nominal.txt.gz | sed s/chr//g | sed s/_/\\t/g | grep -v nan | awk '{print $1 "_" $2 "\t" $0}' | cut -f2,3 --complement | sort -k2,2 -k3,3n >> $sqtl_dir/rpe_glucose_sqtls.txt
+bgzip -f $sqtl_dir/rpe_glucose_sqtls.txt
+tabix -S 1 -s 2 -b 3 -e 3 $sqtl_dir/rpe_glucose_sqtls.txt.gz
 
+zcat /srv/persistent/bliu2/rpe/processed_data/sqtl/fastQTL/nominal/galactose/all.nominal.txt.gz | sed s/chr//g | sed s/_/\\t/g | grep -v nan | awk '{print $1 "_" $2 "\t" $0}' | cut -f2,3 --complement | sort -k2,2 -k3,3n >> $sqtl_dir/rpe_galactose_sqtls.txt
 bgzip -f $sqtl_dir/rpe_sqtls.txt
-tabix -S 1 -s 2 -b 3 -e 3 $sqtl_dir/rpe_sqtls.txt.gz
+tabix -S 1 -s 2 -b 3 -e 3 $sqtl_dir/rpe_galactose_sqtls.txt.gz
 

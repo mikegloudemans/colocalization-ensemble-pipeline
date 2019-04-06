83f180da12fb5c7b18f20ae34da22c7860b2166b
mikegloudemans <mgloud@durga.stanford.edu>
2019-04-05 17:24:56 -0700

    Moved files that aren't really part of this project

--- scripts/auxiliary/eriks_grant/get_positions_for_rsids.sh
+++
@@ -1,3 +0,0 @@
-#join -1 1 -2 3 <(sort rsid_list.txt) <(zcat /users/mgloud/projects/gwas/data/sorted_hg19_snp150.txt.gz) > snps_with_hg19_positions.txt
-join -1 1 -2 3 <(sort insulin_rsids.txt) <(zcat /users/mgloud/projects/gwas/data/sorted_hg19_snp150.txt.gz) | grep -v hap > insulin_rsids_with_hg19_positions.txt
-

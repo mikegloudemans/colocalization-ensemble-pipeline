18c1b8718e7ff3b3a048018a26ffd6c571106535
jeremy29tien <jeremy29tien@gmail.com>
2019-08-02 14:11:12 -0700

    got ensemble working for test example

--- scripts/ensemble.py
+++ scripts/ensemble.py
@@ -11,6 +11,7 @@ else:
    from io import StringIO
 import pandas as pd
 import math
+import numbers
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import csv
@@ -33,47 +34,100 @@ def run_ensemble(locus):
     ## PREPROCESSING ##
     ###################
 
-    # Load matrix of simulated data for training. Consider making this hard-coded in.
+    # Load matrix of simulated data for training. Consider making this hard-coded in -- path is "/users/j29tien/colocalization_ML/colocalization_matrix_NEW_UNRANKED.tsv"
     matrix_save_path = locus.settings["methods"]["ensemble"]["training_data"] # current matrix contains COLOC, RTC, FINEMAP_STANDARD, FINEMAP_MODIFIED, CAVIARBF, BASELINE, SMARTBASELINE, SMR, SMR_HEIDIADJUSTED, GSMR, TWAS 
     matrix = pd.read_csv(matrix_save_path, sep="\t")
 
-    # remove any submethods based on input
+    # remove any submethods based on input and create ScoreContainer numpy array
     methods_used = []
-    if isnumeric(locus.scores.coloc_h4):
+    scores = np.array([])
+    if isinstance(locus.scores.coloc_h4,numbers.Number):
 	methods_used.append("COLOC")
-    if isnumeric(locus.scores.rtc_neg_log_pval): 
+	scores = np.append(scores,locus.scores.coloc_h4)
+    if isinstance(locus.scores.rtc_neg_log_pval,numbers.Number): 
         methods_used.append("RTC")
-    if isnumeric(locus.scores.finemap_clpp): 
+	scores = np.append(scores,locus.scores.rtc_neg_log_pval)
+    if isinstance(locus.scores.finemap_clpp,numbers.Number): 
         methods_used.append("FINEMAP_STANDARD")
-    if isnumeric(locus.scores.finemap_clpp_mod): 
+        scores = np.append(scores,locus.scores.finemap_clpp)
+    if isinstance(locus.scores.finemap_clpp_mod,numbers.Number): 
         methods_used.append("FINEMAP_MODIFIED")
+        scores = np.append(scores,locus.scores.finemap_clpp_mod)
     # CAVIARBF not yet implemented
-    if isnumeric(locus.scores.baseline_neg_log_pval): 
+    if isinstance(locus.scores.baseline_neg_log_pval,numbers.Number): 
         methods_used.append("BASELINE")
-    if isnumeric(locus.scores.smart_baseline_neg_log_pval): 
+        scores = np.append(scores,locus.scores.baseline_neg_log_pval)
+    if isinstance(locus.scores.smart_baseline_neg_log_pval,numbers.Number): 
         methods_used.append("SMARTBASELINE")
-    if isnumeric(locus.scores.smr_neg_log_pval): 
+        scores = np.append(scores,locus.scores.smart_baseline_neg_log_pval)
+    if isinstance(locus.scores.smr_neg_log_pval,numbers.Number): 
         methods_used.append("SMR")
+        scores = np.append(scores,locus.scores.smr_neg_log_pval)
     # SMR_HEIDIADJUSTED not yet implemented
-    if isnumeric(locus.scores.gsmr_neg_log_pval): 
+    if isinstance(locus.scores.gsmr_neg_log_pval,numbers.Number): 
         methods_used.append("GSMR")
-    if isnumeric(locus.scores.twas_neg_log_pval): 
+        scores = np.append(scores,locus.scores.gsmr_neg_log_pval)
+    if isinstance(locus.scores.twas_neg_log_pval,numbers.Number): 
         methods_used.append("TWAS")
+        scores = np.append(scores,locus.scores.twas_neg_log_pval)
     
+    methods_used.append("colocalization_status") # always include answer key
     matrix = matrix[methods_used]
+
+    print(methods_used)
 
     # use all data in matrix for training (no need for train test split)
     trainX = matrix.drop('colocalization_status', 1)
     trainY = matrix['colocalization_status']
+    
+    #convert training and testing data to numpy arrays
+    trainX = trainX.to_numpy()  # access the numpy array containing values
+    trainY = trainY.to_numpy()  # access the numpy array containing values
+    
+    # Binarize the output for training
+    #trainY = label_binarize(trainY, classes=[0, 1])
+    #n_classes = trainY.shape[1]
+
+    ####################
+    ## TRAIN ENSEMBLE ##
+    ####################
+    
+    # initiate and fit the random forest classifier -- need to tweak parameters --> n_estimators = 150, max_depth = 7, min_samples_leaf=1
+    #randfor = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=7, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=150, n_jobs=None, oob_score=False, random_state=None, verbose=0, warm_start=False)
+    
+    # for sklearn version < 0.20.0, remove min_impurity_decrease=0.0, set min_impurity_split=0, remove n_jobs=None    
+    randfor = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=7, max_features='auto', max_leaf_nodes=None, min_impurity_split=0, min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=150, oob_score=False, random_state=None, verbose=0, warm_start=False)
+    
+    randfor.fit(trainX, trainY)
+    
+
+    ###############################
+    ## COLOCALIZATION PREDICTION ##
+    ###############################
+    
+    #predict using real data
+    print(scores)
+    # for sklearn version < 0.20.0, scores.reshape(1,-1) is necessary
+    scores = scores.reshape(1,-1)
+
+    predY = randfor.predict(scores)
+    print(predY)
+    predProbY = randfor.predict_proba(scores)
+    print("COLOCALIZATION SCORE:")
+    print(predProbY[:,1])
+    
 
     # TODO: Set this value equal to the ensemble score once you've computed it, 
     # and then it will be written to the output file.
-    ensemble_score = 0
+    ensemble_score = predProbY[0,1]
 
     # TODO for Jeremy: Write the names of all the methods / scores that were included (not NoneType) in this file so we can
     # reference it later
+    methods_used.remove('colocalization_status')
     with open("{0}/ensemble_methods_used.txt".format(locus.basedir, locus.gwas_suffix.replace(".", "_")), "a") as w:
-        pass
+	for method in methods_used:
+            w.write("%s\t" % method)       
+	w.write("\n") 
 
     with open("{0}/{1}_ensemble_status.txt".format(locus.basedir, locus.gwas_suffix.replace(".", "_")), "a") as a:
         a.write("{0}_{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n".format(locus.chrom, locus.pos, locus.eqtl_suffix, locus.trait, locus.gene, locus.data.shape[0], locus.gwas_suffix, ensemble_score))

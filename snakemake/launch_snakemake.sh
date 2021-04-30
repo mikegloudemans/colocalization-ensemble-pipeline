8aa074cb6822ae94c4d86ad6878712506501c425
Nicole Gay <nicolerg@stanford.edu>
2021-04-30 14:58:23 -0700

    abandon group ex

---
+++ snakemake/launch_snakemake.sh
@@ -0,0 +1,23 @@
+# cluster execution
+snakemake -j 100 --cluster-config slurm-config.json \
+					--latency-wait 90 \
+					--configfile colocalization-config.json \
+					--cluster \
+					"sbatch --account={cluster.account} \
+						--partition={cluster.partition} \
+						--time={cluster.time} \
+						--mem={cluster.mem} \
+						--cpus-per-task={cluster.nCPUs} \
+						--mail-type={cluster.mail} \
+						--output={cluster.output}" 
+
+# dry run
+snakemake -n --configfile colocalization-config.json
+
+# execute on current node
+snakemake -j 1 --configfile colocalization-config.json
+
+# print DAG to home
+# this won't work if the snakefile prints anything to stdout
+snakemake -j 1 --configfile colocalization-config.json --forceall | dot -Tpdf > ~/dag.pdf
+
